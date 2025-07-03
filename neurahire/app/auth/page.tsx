adaf421492180fae3372fd1b3f06305b512a26aa
mateenshamsi <matinshamsi16@gmail.com>
2025-07-03 19:13:02 +0530

    Proper url

--- neurahire/app/auth/page.tsx
+++ neurahire/app/auth/page.tsx
@@ -9,7 +9,7 @@ function Login() {
     const {error}= await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-    redirectTo: process.env.NEXT_PUBLIC_REDIRECT_URL || 'http://localhost:3000/auth/callback',
+    redirectTo: process.env.NEXT_PUBLIC_HOST_URL || 'http://localhost:3000/auth/callback',
   }
     })
     if(error){
